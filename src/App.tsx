ecc6c8a8c53241f91189bd47cd8bb2f384317747
natchanon pornprasatpol <natchanon.por@cpf.co.th>
2023-02-16 09:46:55 +0700

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react {logic(1,20)}
+          hellow world!! learn react {logic(1,20)}
         </a>
       </header>
     </div>
